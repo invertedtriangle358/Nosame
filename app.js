ac83449376a8828bd14062a7c29142652f02e80e
逆砂 参角 <135438665+invertedtriangle358@users.noreply.github.com>
2025-09-20 02:45:39 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const MAX_POST_LENGTH = 108;
 const NG_WORDS = [
   "キチガイ", "ガイジ", "ケンモ", "嫌儲", "右翼", "左翼", "ウヨ", "サヨ", "パヨク",
-  "与党", "野党", "在日", "クルド", "死ね", "殺す", "クソ", "fuck", "shit",
-  "sex", "porn", "gay", "ass", "dick", "pussy", "CP", "mempool",
+  "与党", "野党", "在日", "クルド", "死ね", "殺す", "クソ", "ログボ", "チカラコブ", "スジャータ", "ｴﾄﾞｳｨﾝ", "かまどのお菓子", "fuck", "shit",
+  "sex", "porn", "gay", "ass", "dick", "pussy", "CP", "mempool", "Bottlesky",
   "http://", "https://"
 ];
 const DEFAULT_RELAYS = [
