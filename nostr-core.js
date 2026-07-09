5e426e198807115e5a7760948af71ef591171edd
逆砂 参角 <135438665+invertedtriangle358@users.noreply.github.com>
2026-07-09 23:33:55 +0900

    Update nostr-core.js

--- nostr-core.js
+++ nostr-core.js
@@ -1,5 +1,3 @@
-import { CONFIG, NOSTR_KINDS, UI_STRINGS } from "./config.js";
-import { validateEvent, verifyEvent, getEventHash } from "https://esm.sh/nostr-tools@2";
 export { NostrCodec } from "./nostr-codec.js";
 export { EventValidator } from "./nostr-validator.js";
 export { StorageManager, ProfileStore } from "./nostr-stores.js";
