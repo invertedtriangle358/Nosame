80fefe7b0326e017fc52f3953f26d322f59c243f
逆砂 参角 <135438665+invertedtriangle358@users.noreply.github.com>
2026-07-17 10:00:52 +0900

    Update nostr-core.js

--- nostr-core.js
+++ nostr-core.js
@@ -1,4 +1,5 @@
 export { NostrCodec } from "./nostr-codec.js";
+export { EventReference } from "./event-reference.js";
 export { EventValidator } from "./nostr-validator.js";
 export { StorageManager, ProfileStore } from "./nostr-stores.js";
 export { NostrClient } from "./nostr-client.js";
